d8e827f90776d267d26d23783063cf08e7659aaa
Chadwick Czech <kochanov@lanbilling.ru>
2024-05-02 13:53:17 +0300

    1. Исправлена ошибка, когда отклики отсылались с 2-х аккаунтов в одно время.  access_token был последнего аккаунта. Теперь access_token передается в agenda 2. Поправил текст сообщения, приходящего на почту. Теперь там есть описание и ссылка на работодателя

--- server/router/api/schedule/schedule.ts
+++ server/router/api/schedule/schedule.ts
@@ -1,116 +1,166 @@
-import { Request, Response } from "express";
-import sendMail from "../../../utils/mailer";
-import { api } from "..";
-import { agenda } from "../../..";
+import { Request, Response } from 'express';
+import sendMail from '../../../utils/mailer';
+import { api } from '..';
+import { agenda } from '../../..';
+import axios from 'axios';
+
+export type stackT = {
+    name: string;
+    info: {
+        requirement?: string;
+        responsibility?: string;
+    };
+    link: string;
+    employer: { name: string; link: string };
+};
 
 const schedule = async (req: Request, res: Response) => {
-  const { hours, minutes, count, resume_id, message, email } = req.body;
+    const { hours, minutes, resume_id, message, email } = req.body;
 
-  let stack: { name: string; info: string; link: string }[] = [];
+    let stack: stackT[] = [];
 
-  let queue = [];
+    let queue = [];
 
-  // 1. Смотрю есть ли уже запущенная таска
-  const job = await agenda.jobs({ name: resume_id });
+    // 1. Смотрю есть ли уже запущенная таска
+    const job = await agenda.jobs({ name: resume_id });
 
-  if (!job.length) {
-    // 2. Если нет, то создаю таску с названием айди пользователя
+    if (!job.length) {
+        // 2. Если нет, то создаю таску с названием айди пользователя
 
-    agenda.define(resume_id, async (job, done) => {
-      try {
-        let page = 1;
+        agenda.define(resume_id, async (job, done) => {
+            try {
+                let page = 0;
 
-        // 3. Получаю 300 вакансий. Чтобы при максимально возможных запланированных откликах меньше был шанс того что вакансий без тестового задания будет меньше
-        while (page < 4) {
-          const response = await api
-            .getData(
-              `resumes/${resume_id}/similar_vacancies?page=${page}&per_page=100`
-            )
-            .then((response) => response.data)
-            .catch((err) => {
-              res.status(403).send(err);
-            });
+                // 3. Получаю 300 вакансий. Чтобы при максимально возможных запланированных откликах меньше был шанс того что вакансий без тестового задания будет меньше
+                while (page < 3) {
+                    const response = await axios
+                        .get(
+                            `https://api.hh.ru/resumes/${resume_id}/similar_vacancies?page=${page}&per_page=100`,
+                            {
+                                headers: {
+                                    // 'Content-Type': 'application/json',
+                                    'Content-Type': 'multipart/form-data',
 
-          // 4. фильтрую вакансии по тестовому
-          const vanacies_with_no_test = response.items.filter(
-            (el) => !el.has_test
-          );
+                                    'User-Agent':
+                                        'HHbot (a.kochanov31@yandex.ru)',
+                                    Authorization: `Bearer ${req.cookies.access_token}`
+                                },
+                                withCredentials: true
+                            }
+                        )
+                        .then(response => response.data)
+                        .catch(err => {
+                            console.log(
+                                `ERR IN FILTER VACANCIES BY HAS_TEST`,
+                                err
+                            );
+                            job.fail(err.data);
+                            res.status(403).send(err);
+                        });
 
-          queue = queue.concat(vanacies_with_no_test);
+                    // 4. фильтрую вакансии по тестовому
+                    const vanacies_with_no_test = response.items.filter(
+                        el => !el.has_test
+                    );
 
-          page++;
-        }
+                    queue = queue.concat(vanacies_with_no_test);
 
-        // 5. Итерируюсь по вакансиям и отправляю отклик (так же как и на клиенте)
-        for (let i = 0; i < count; i++) {
-          // 6. Беру вакансию из очереди
-          const vacancy = queue[i];
+                    page++;
+                }
 
-          if (
-            (vacancy.response_letter_required && message) ||
-            !vacancy.response_letter_required
-          ) {
-            const formData = new FormData();
+                // 5. Итерируюсь по вакансиям и отправляю отклик (так же как и на клиенте)
+                for (let i = 0; i < req.body.count; i++) {
+                    // 6. Беру вакансию из очереди
+                    const vacancy = queue[i];
 
-            formData.append("vacancy_id", `${vacancy.id}`);
-            formData.append("resume_id", resume_id);
+                    if (
+                        (vacancy.response_letter_required && message) ||
+                        !vacancy.response_letter_required
+                    ) {
+                        const formData = new FormData();
 
-            if (message) {
-              formData.append("message", message);
+                        formData.append('vacancy_id', `${vacancy.id}`);
+                        formData.append('resume_id', resume_id);
+
+                        if (message) {
+                            formData.append('message', message);
+                        }
+
+                        await axios
+                            .post('https://api.hh.ru/negotiations', formData, {
+                                headers: {
+                                    // 'Content-Type': 'application/json',
+                                    'Content-Type': 'multipart/form-data',
+
+                                    'User-Agent':
+                                        'HHbot (a.kochanov31@yandex.ru)',
+                                    Authorization: `Bearer ${req.cookies.access_token}`
+                                },
+                                withCredentials: true
+                            })
+                            .then(res => {
+                                // 5. Успешные отклики сохраняю в массив
+                                if (res.status === 201 || res.status === 303) {
+                                    stack.push({
+                                        name: vacancy.name,
+                                        info: {
+                                            requirement:
+                                                vacancy.snippet.requirement ||
+                                                '',
+                                            responsibility:
+                                                vacancy.snippet
+                                                    .responsibility || ''
+                                        },
+                                        link: vacancy.alternate_url,
+                                        employer: {
+                                            name: vacancy.employer.name,
+                                            link: vacancy.employer.alternate_url
+                                        }
+                                    });
+                                }
+                            })
+                            .catch(err => {
+                                // Если отклик с ошибкой то сохраняю в бд.
+                                job.fail(err.data);
+                                console.log(`ERR IN SEND RESPONSE `, err);
+                            });
+                    }
+                }
+            } catch (err) {
+                job.fail(err.data);
+                console.log(`ERR IN AGENDA`, err);
             }
 
-            await api
-              .postData("negotiations", formData)
-              .then(() => {
-                // 5. Успешные отклики сохраняю в массив
-                stack.push({
-                  name: vacancy.name,
-                  info: vacancy.snippet.requirement,
-                  link: vacancy.alternate_url,
-                });
-              })
-              .catch((err) => {
-                // Если отклик с ошибкой то сохраняю в бд.
-                job.fail(err.data);
-                console.log(`ERR IN SEND RESPONSE `, err);
-              });
-          }
+            job.save();
+            done();
+        });
+
+        // 6. Запуск таски каждый день в указанное время
+
+        await agenda.every(`${minutes} ${hours} * * *`, resume_id, req.body, {
+            timezone: 'Europe/Moscow'
+        });
+
+        agenda.on('complete', job => {
+            // 7. По завршению таски и если есть отклики то отправляю сообщение на почту
+            if (job.attrs.name === resume_id && stack.length) {
+                sendMail(email, stack);
+            }
+        });
+
+        async function graceful() {
+            await agenda.stop();
+            await agenda.cancel({ name: resume_id });
+            process.exit(0);
         }
-      } catch (err) {
-        job.fail(err.data);
-        console.log(`ERR IN AGENDA`, err);
-      }
 
-      job.save();
-      done();
-    });
+        process.on('SIGTERM', graceful);
+        process.on('SIGINT', graceful);
 
-    // 6. Запуск таски каждый день в указанное время
-
-    await agenda.every(`${minutes} ${hours} * * *`, resume_id, req.body, {
-      timezone: "Europe/Moscow",
-    });
-
-    agenda.on("complete", (job) => {
-      // 7. По завршению таски и если есть отклики то отправляю сообщение на почту
-      if (job.attrs.name === resume_id && stack.length) {
-        sendMail(email, stack);
-      }
-    });
-
-    async function graceful() {
-      await agenda.stop();
-      await agenda.cancel({ name: resume_id });
-      process.exit(0);
+        res.status(200).send('Автоотклики успешно запланированны');
+    } else {
+        res.status(409).send('Автоотклики уже запланированны');
     }
-
-    process.on("SIGTERM", graceful);
-    process.on("SIGINT", graceful);
-
-    res.status(200).send("Автоотклики успешно запланированны");
-  } else {
-    res.status(409).send("Автоотклики уже запланированны");
-  }
 };
 
 export default schedule;
--- server/router/api/schedule/scheduleRouter.ts
+++ server/router/api/schedule/scheduleRouter.ts
@@ -1,26 +1,33 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import schedule from "./schedule";
-import { agenda } from "../../..";
+import schedule from './schedule';
+import { agenda } from '../../..';
+import auth from '../../../middlewares/auth';
 
 export function scheduleRouter(router: Router) {
-  router.post("/api/schedule", schedule);
-  router.get("/api/schedule/:id", async (req, res) => {
-    const jobs = await agenda.jobs({ name: req.params.id });
+    router.post('/api/schedule', auth, schedule);
+    router.get('/api/schedule/:id', async (req, res) => {
+        const jobs = await agenda.jobs({
+            name: req.params.id
+        });
 
-    if (jobs.length) {
-      res.send(jobs[0]);
-    } else {
-      res.send("Нет запланированных откликов");
-    }
-  });
-  router.delete("/api/schedule/:id", async (req, res) => {
-    const jobs = await agenda.jobs({ name: req.params.id });
-    if (jobs.length) {
-      agenda.cancel({ name: req.params.id });
-      res.status(200).send("OK");
-    } else {
-      res.status(404).send("Задача не найдена");
-    }
-  });
+        if (jobs.length) {
+            res.send(jobs[0]);
+        } else {
+            res.send('Нет запланированных откликов');
+        }
+    });
+    router.delete('/api/schedule/:id', async (req, res) => {
+        const jobs = await agenda.jobs({
+            name: req.params.id
+        });
+        if (jobs.length) {
+            agenda.cancel({
+                name: req.params.id
+            });
+            res.status(200).send('OK');
+        } else {
+            res.status(404).send('Задача не найдена');
+        }
+    });
 }
